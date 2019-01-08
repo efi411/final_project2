92547cbdd21bc4de3d5f2549bd5cf79265a01009
i345010 <34436035+i345010@users.noreply.github.com>
2019-01-08 15:15:49 +0200

    Update main.go

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ func main() {
 
 	//Create the manager and start the game
 	manager := m.GetInstance()
-
 	//Arguments: Number of players, Probability of loosing messages
 	if err := manager.StartGame(5, 1); err != nil {
 		fmt.Println("Something went wrong!")
